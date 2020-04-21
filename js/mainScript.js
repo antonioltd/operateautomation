42cd59b9ac8a5b8b2d30ae92d054bdd35e2e8f08
tim-brown-azure <tim.brown.azure@gmail.com>
2020-04-21 21:15:26 +0100

    add validation fields and warning test message; remove alerts;

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,25 +1,61 @@
 var attempt = 3;
+var username;
+var password;
+var usernameErrMsg;
+var passwordErrMsg;
+var warningMessage;
+var genError;
+
 function validate() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if (username.toLowerCase() == "admin" && password == "admin") {
-    alert("Login successfully");
-    window.location.href = "practice.html";
+  username = document.getElementById("username");
+  password = document.getElementById("password");
+  usernameErrMsg = document.getElementById("username-error");
+  passwordErrMsg = document.getElementById("password-error");
+  if (username.value.toLowerCase() == "admin" && password.value == "admin") {
+    navigateTo("practice.html");
+
     return false;
   } else {
+    if (username.value === "") {
+      usernameErrMsg.innerHTML = "Username cannot be empty.";
+    }
+    if (password.value === "") {
+      passwordErrMsg.innerHTML = "Password cannot be empty.";
+    }
+
+    if (username.value !== "" && password.value !== "") {
+      genError = document.getElementById("general-error");
+      genError.innerHTML = "Login details not found!";
+    }
     attempt--; // Decrementing by one.
-    alert("You have left " + attempt + " attempt;");
-    // Disabling fields after 3 attempts.
+    warningMessage = document.getElementById("warning-count-message");
+    if (attempt > 0) {
+      warningMessage.innerHTML =
+        "You have " + attempt + " attempt(s) remaining!!! ";
+    }
     if (attempt == 0) {
-        window.location.href = "fallout.html";
-      document.getElementById("username").disabled = true;
-      document.getElementById("password").disabled = true;
-      document.getElementById("login").disabled = true;
+      warningMessage.innerHTML = "Loading.....";
+
+      setTimeout(function () {
+        navigateTo("fallout.html");
+      }, 3000);
       return false;
     }
   }
 }
 
-function buttonClick() {
-  alert("try");
+function usernameClick() {
+  clearUsernameErrorMessage();
+}
+
+function clearUsernameErrorMessage() {
+  usernameErrMsg.innerHTML = "";
+}
+
+function clearPasswordErrorMessag() {
+  passwordErrMsg.innerHTML = "";
+}
+
+function navigateTo(url) {
+  window.location.href = url;
 }
